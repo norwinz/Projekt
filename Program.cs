053d5a54c8b526f945962fc3aaca15c7f2a775f4
norwinz <dan-gustafsson@hotmail.com>
2021-10-08 09:32:26 +0200

    Added description

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿/*
+                --Description of program and its purpose--
+            Author: Dan Gustafsson, SUT21.
+            "Individuellt projekt - Enkel internetbank/bankomat"
+            Final project of "Utveckling med C# och .NET"
+*/
+using System;
 
 namespace Projekt
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //MAIN PART: Login and menu
         {
-            //File created and uploaded to Github
+            
         }
     }
 }
