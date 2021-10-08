0b15fdde59e2eb1e24bdd87b4e8d75df4665f158
norwinz <dan-gustafsson@hotmail.com>
2021-10-08 09:07:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File created and uploaded to Github
+        }
+    }
+}
